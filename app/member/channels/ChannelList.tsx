29735118f1be2c1b62533a352ae7bca4ec76b99e
Damian Barszcz <mail.damianbarszcz@gmail.com>
2025-05-27 01:09:38 +0200

    update: data for ChannelList component

--- app/member/channels/ChannelList.tsx
+++ app/member/channels/ChannelList.tsx
@@ -6,14 +6,16 @@ interface ChannelListProps {
 }
 
 const ChannelList: React.FC<ChannelListProps> = ({ teams }) => {
+
     return (
         <section className="w-full">
             <div className="m-auto max-w-6xl">
                 <div className="grid grid-cols-3 mt-10 mb-20 w-full gap-6">
                     {teams.map(team => (
+
                     <div key={team.id} className="flex flex-col justify-between global--border-d-white rounded-md p-4">
                         <div className="flex content-center items-center">
-                            <figure className="relative w-16 h-16 bg-amber-200 rounded-lg">
+                            <figure className="relative w-16 h-16 rounded-lg" style={{ backgroundColor: team.team_details.team_avatar_color }}>
                                 <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-body font-semibold text-base">RS</span>
                             </figure>
                             <div className="ml-5">
@@ -38,8 +40,8 @@ const ChannelList: React.FC<ChannelListProps> = ({ teams }) => {
                             <div className="mt-10 flex justify-end">
                                 <Link href="/member/channels/manage" className="block pt-2 pb-2 pl-6 pr-6 mr-5 global--border-silver rounded-3xl
                                     text-sm font-body font-semibold global--text-silver" target="_self"> Zarządzaj </Link>
-                                <Link href="/member/channels/channel" className="block pt-2 pb-2 pl-6 pr-6 global--border-blue rounded-3xl
-                                    text-sm font-body font-semibold global--text-link" target="_self"> Dołącz </Link>
+                                <a href={`/member/channels/channel/${team.slug}`} className="block pt-2 pb-2 pl-6 pr-6 global--border-blue rounded-3xl
+                                    text-sm font-body font-semibold global--text-link" target="_self"> Dołącz </a>
                             </div>
                         </div>
                     </div>
